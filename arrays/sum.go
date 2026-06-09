ecd32fce5444f58cd585f554e709e6cb95c42dca
tortillaproduction <momomo0206@gmail.com>
2026-06-09 19:19:07 +0900

    refactor: change SumAll function and test to SumAllTails with test

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,12 +8,15 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
