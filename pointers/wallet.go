23985a913002463b3ce9111f6cfcfa4223245942
tortillaproduction <momomo0206@gmail.com>
2026-06-10 09:32:12 +0900

    feat: implementation Wallet struct with test

---
+++ pointers/wallet.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
