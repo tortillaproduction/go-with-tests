beebab5dfc1bf0d2f2d7b092b1e8d21ee8d9c0c5
tortillaproduction <momomo0206@gmail.com>
2026-06-10 14:11:31 +0900

    refactor: added error as an argument to the Search method and modified the test accordingly

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,26 @@ import "testing"
 func TestSerach(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Serach("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertStrings(t *testing.T, got, want string) {
