d24a7cb2ccdad872fe27d85ca7226a6d180584e6
tortillaproduction <momomo0206@gmail.com>
2026-06-13 08:23:28 +0900

    refactor: modify using defer in test

--- select/racer_test.go
+++ select/racer_test.go
@@ -11,6 +11,9 @@ func TestRacer(t *testing.T) {
 	slowServer := makeDelayedServer(20 * time.Millisecond)
 	fastServer := makeDelayedServer(0 * time.Millisecond)
 
+	defer slowServer.Close()
+	defer fastServer.Close()
+
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
 
@@ -20,9 +23,6 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
-
-	slowServer.Close()
-	fastServer.Close()
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
