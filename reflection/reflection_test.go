35e75f6b0fe93d51fe458f064cef80590c1a9466
tortillaproduction <momomo0206@gmail.com>
2026-06-14 20:46:13 +0900

    refactor: add the case for channels

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -112,6 +112,27 @@ func TestWalk(t *testing.T) {
 		assertContains(t, got, "Bar")
 		assertContains(t, got, "Boz")
 	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 
 func assertContains(t *testing.T, heystack []string, needle string) {
