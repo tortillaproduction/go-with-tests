9f11838d2f31f099577d79fccd4bee24b2bac86b
tortillaproduction <momomo0206@gmail.com>
2026-06-16 22:10:51 +0900

    refactor: increase test cases and modify test fn

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,21 @@ package v1
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q want %q", got, test.Want)
+			}
+		})
 	}
 }
--- roman-numerals/roman-numerals.go
+++ roman-numerals/roman-numerals.go
@@ -1,5 +1,8 @@
 package v1
 
 func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
 	return "I"
 }
