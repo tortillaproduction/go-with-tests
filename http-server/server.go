987557081326da298e6621202659f51d29c39b80
tortillaproduction <momomo0206@gmail.com>
2026-06-21 21:14:52 +0900

    refactor: implementation PlayerStore interface and stub in test

--- http-server/server.go
+++ http-server/server.go
@@ -6,20 +6,27 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-	fmt.Fprint(w, GetPlayerScore(player))
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
+// func GetPlayerScore(name string) string {
+// 	if name == "Pepper" {
+// 		return "20"
+// 	}
 
-	if name == "Floyd" {
-		return "10"
-	}
+// 	if name == "Floyd" {
+// 		return "10"
+// 	}
 
-	return ""
-}
+// 	return ""
+// }
