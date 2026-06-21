3a75ac6d38554ee81a587d7f442bc37dbe58c936
tortillaproduction <momomo0206@gmail.com>
2026-06-21 21:34:57 +0900

    refactor: add inmemory store and build

--- http-server/main.go
+++ http-server/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	server := &PlayerServer{}
+type InMemoryPlayerStore struct{}
 
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	fmt.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
