a579a7d1b483479f92e1d62aa2033f43eff0821d
tortillaproduction <momomo0206@gmail.com>
2026-06-22 21:44:20 +0900

    refactor: add POST method route with test

--- http-server/server.go
+++ http-server/server.go
@@ -15,6 +15,15 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	score := p.store.GetPlayerScore(player)
@@ -23,17 +32,9 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	fmt.Fprint(w, score)
 }
 
-// func GetPlayerScore(name string) string {
-// 	if name == "Pepper" {
-// 		return "20"
-// 	}
-
-// 	if name == "Floyd" {
-// 		return "10"
-// 	}
-
-// 	return ""
-// }
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
