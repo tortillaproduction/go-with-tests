67dd618390d793075821f5c505c3ddbcf7f89852
tortillaproduction <momomo0206@gmail.com>
2026-06-24 21:22:33 +0900

    refactor: adding GetPlayerScore method in file system store with test

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -8,6 +8,19 @@ type FileSystemPlayerStore struct {
 	database io.ReadSeeker
 }
 
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
+
 func (f *FileSystemPlayerStore) GetLeague() []Player {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
