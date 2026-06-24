2f08a0a418c0edcbe4c4a4c88f0fd5ba3433e6cb
tortillaproduction <momomo0206@gmail.com>
2026-06-24 21:46:23 +0900

    refactor: adding RecordWin method in file system store with test

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 )
 
@@ -8,6 +9,7 @@ type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
 }
 
+// --- methods ---
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 	var wins int
 
@@ -19,6 +21,19 @@ func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 	}
 
 	return wins
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+		}
+	}
+
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
