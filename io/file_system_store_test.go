357002233b9ce75e9e0762ecde7586e600bfe9d0
tortillaproduction <momomo0206@gmail.com>
2026-06-26 22:51:34 +0900

    refactor: adding tape struct with test

--- io/file_system_store_test.go
+++ io/file_system_store_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"os"
 	"testing"
 )
@@ -78,7 +77,7 @@ func TestFileSystemStore(t *testing.T) {
 }
 
 // --- helpers ---
-func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
 	t.Helper()
 
 	tmpfile, err := os.CreateTemp("", "db")
