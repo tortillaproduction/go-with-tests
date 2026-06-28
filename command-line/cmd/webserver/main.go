f7c2e1d8ca10b592dbc65211f70c23da85b0109a
tortillaproduction <momomo0206@gmail.com>
2026-06-28 20:43:38 +0900

    refactor: change folder structure and adding poker package with test

---
+++ command-line/cmd/webserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	poker "github.com/quii/learn-go-with-tests/command-line/v1"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0o666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v", err)
+	}
+
+	server := poker.NewPlayerServer(store)
+
+	fmt.Println("Listening on port 5000")
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tortillaproduction/go-with-tests
 
 go 1.25.0
+
+require github.com/quii/learn-go-with-tests v0.0.0-20260509070331-dc00c650f666
