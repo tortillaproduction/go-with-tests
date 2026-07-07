cd99e980711a103c9db6277bfa40708793ec9807
tortillaproduction <69611969+tortillaproduction@users.noreply.github.com>
2026-07-07 08:16:39 +0000

    refactor: adding section and logic

--- websockets/game.go
+++ websockets/game.go
@@ -1,6 +1,8 @@
 package poker
 
+import "io"
+
 type Game interface {
-	Start(numberOfPlayers int)
+	Start(numberOfPlayers int, alertDestination io.Writer)
 	Finish(winner string)
 }
--- websockets/testing.go
+++ websockets/testing.go
@@ -2,38 +2,39 @@ package poker
 
 import (
 	"fmt"
+	"io"
 	"testing"
 	"time"
 )
 
 type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
+	Scores   map[string]int
+	WinCalls []string
+	League   []Player
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
+	score := s.Scores[name]
 	return score
 }
 
 func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
+	s.WinCalls = append(s.WinCalls, name)
 }
 
 func (s *StubPlayerStore) GetLeague() League {
-	return s.league
+	return s.League
 }
 
 func AssertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
 	t.Helper()
 
-	if len(store.winCalls) != 1 {
-		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	if len(store.WinCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.WinCalls), 1)
 	}
 
-	if store.winCalls[0] != winner {
-		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	if store.WinCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.WinCalls[0], winner)
 	}
 }
 
@@ -50,6 +51,6 @@ type SpyBlindAlerter struct {
 	Alerts []ScheduledAlert
 }
 
-func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int) {
+func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int, to io.Writer) {
 	s.Alerts = append(s.Alerts, ScheduledAlert{at, amount})
 }
